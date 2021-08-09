37a0a2d6cc6eceb6b94936fa93d858395378050c
pavelqq <pavel.schardakov@gmail.com>
2021-08-10 00:12:03 +0500

    remove yarn.lock and package-json.lock

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, "./build/index.html"));
+    res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
 app.listen(port, () => {
